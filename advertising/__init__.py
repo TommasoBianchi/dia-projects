9f7a9ac0c468a74a4c2d8f51577d4ba5dc8ca9bd
Raffaele Bongo <raffaele.bongo@mail.polimi.it>
2019-05-11 14:21:17 +0200

    - knapsack.py: working

--- advertising/__init__.py
+++ advertising/__init__.py
@@ -1,6 +1,6 @@
 from advertising.knapsack.knapsack import Knapsack
 
-values = [[1,2,5], [7,88,4], [5,4,2]]
+values = [[1,2,5,7], [7,15,4,7], [5,4,2,12]]
 algo = Knapsack(3,3,values)
 
 result = algo.optimize()
--- advertising/knapsack/knapsack.py
+++ advertising/knapsack/knapsack.py
@@ -7,16 +7,20 @@ class Knapsack:
         self.values = values
 
     def optimize(self):
-        results = [[0]*self.budgets]*(self.subcampaigns + 1)
+        results = [[0]*self.budgets for _ in range(self.subcampaigns + 1)]
         return self.knapsack_optimization(results, 1, 1, 1, self.budgets, self.budgets)
 
     def knapsack_optimization(self, results, ind_value_row, ind_value_col, ind_res_row, ind_res_col, ind_res_col_curr):
         # Base case 1
-        if ind_res_row == self.subcampaigns + 1 and ind_res_col == 0:
+        if ind_res_row == self.subcampaigns and ind_res_col == 1:
             return results
 
-        if ind_res_col_curr == 0:
-            return self.knapsack_optimization(results, ind_value_row + 1, 0, ind_res_row + 1, ind_res_col - 1, ind_res_col - 1)
+        elif ind_res_col_curr == 0 and ind_res_col == 1:
+            return self.knapsack_optimization(results, ind_value_row + 1, 1, ind_res_row + 1,
+                                              self.budgets, self.budgets)
+
+        elif ind_res_col_curr == 0:
+            return self.knapsack_optimization(results, ind_value_row, 1, ind_res_row, ind_res_col - 1, ind_res_col - 1)
 
         composed_value = self.values[ind_value_row - 1][ind_value_col - 1] + results[ind_res_row - 1][ind_res_col_curr - 1]
 

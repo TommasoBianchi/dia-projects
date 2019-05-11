27e4c8522684b7a98b3c4c90c5778cf0da948159
Raffaele Bongo <raffaele.bongo@mail.polimi.it>
2019-05-11 14:25:27 +0200

    - knapsack.py: commented

--- advertising/knapsack/knapsack.py
+++ advertising/knapsack/knapsack.py
@@ -11,17 +11,20 @@ class Knapsack:
         return self.knapsack_optimization(results, 1, 1, 1, self.budgets, self.budgets)
 
     def knapsack_optimization(self, results, ind_value_row, ind_value_col, ind_res_row, ind_res_col, ind_res_col_curr):
-        # Base case 1
+        # Base case: all the subcampaings have been evaluated
         if ind_res_row == self.subcampaigns and ind_res_col == 1:
             return results
 
+        # Happens when the optimization step of the current subcampaing have been terminated
         elif ind_res_col_curr == 0 and ind_res_col == 1:
             return self.knapsack_optimization(results, ind_value_row + 1, 1, ind_res_row + 1,
                                               self.budgets, self.budgets)
 
+        # Happens when we need to perform the optimization for the next budget in a given subcampaing
         elif ind_res_col_curr == 0:
             return self.knapsack_optimization(results, ind_value_row, 1, ind_res_row, ind_res_col - 1, ind_res_col - 1)
 
+        # Perform the optimization for a given budget
         composed_value = self.values[ind_value_row - 1][ind_value_col - 1] + results[ind_res_row - 1][ind_res_col_curr - 1]
 
         if composed_value > results[ind_res_row][ind_res_col - 1]:

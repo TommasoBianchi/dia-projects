2c2e7796fe1ed110549e2c661be07cf98a7d4572
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-05-14 11:24:09 +0200

    Fixed edge update (now done less often)

--- matching/graph/edge.py
+++ matching/graph/edge.py
@@ -5,7 +5,15 @@ class Edge:
         self.node1 = min([node1, node2], key = lambda n: n.node_class.id)
         self.node2 = max([node1, node2], key = lambda n: n.node_class.id)
 
+        self.last_weight_estimate_sample_size = -1
+
     # Update the estimated weight of the edge.
     def update_weight(self):
         edge_data = self.node1.node_class.edge_data[self.node2.node_class.id]
-        self.weight = edge_data.distribution.sample() * edge_data.estimated_weight
+
+        # Update the estimated weight only if we have more data on which to estimate
+        # This is especially important for TS, as it avoids constantly taking new samples
+        # from the Beta distribution that will make the like of DDA much more complicated
+        if self.last_weight_estimate_sample_size != edge_data.weight_estimation_samples:
+        	self.weight = edge_data.distribution.sample() * edge_data.estimated_weight
+        	self.last_weight_estimate_sample_size = edge_data.weight_estimation_samples

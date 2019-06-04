37219556124e980274bb59ed8c846a5150283f8f
Nontrovounnick <giovanni.probo@mail.polimi.it>
2019-06-04 13:04:14 +0200

    Fixed run.py

--- advertising/run.py
+++ advertising/run.py
@@ -1,9 +1,9 @@
-from environment.click_function import Click_Function
-from distributions.gaussian import Gaussian
-from environment.subcampaign import Subcampaign
-from environment.environment import Environment
-from knapsack.knapsack import Knapsack
-from experiment.subcampaign_algo import Subcampaign_algo
+from advertising.environment.click_function import Click_Function
+from advertising.distributions.gaussian import Gaussian
+from advertising.environment.subcampaign import Subcampaign
+from advertising.environment.environment import Environment
+from advertising.knapsack.knapsack import Knapsack
+from advertising.experiment.subcampaign_algo import Subcampaign_algo
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -16,8 +16,8 @@ subcampaign_2 = Subcampaign([class_2])
 
 environment = Environment([class_1, class_2])
 
-subcampaign_algo_1 = Subcampaign_algo(4,[0,10,20,30])
-subcampaign_algo_2 = Subcampaign_algo(4,[0,10,20,30])
+subcampaign_algo_1 = Subcampaign_algo([0,10,20,30])
+subcampaign_algo_2 = Subcampaign_algo([0,10,20,30])
 subcampaign_algos = [subcampaign_algo_1, subcampaign_algo_2]
 
 rewards = []
@@ -25,9 +25,9 @@ rewards = []
 for t in range(100):
     subcampaign_algo_1_values = [subcampaign_algo_1.sample_from_gp(arm) for arm in [0,10,20,30]]
     subcampaign_algo_2_values = [subcampaign_algo_2.sample_from_gp(arm) for arm in [0,10,20,30]]
-    knapsack = Knapsack(3, 30, [subcampaign_algo_1_values, subcampaign_algo_2_values])
+    knapsack = Knapsack(30, [subcampaign_algo_1_values, subcampaign_algo_2_values])
     super_arm = knapsack.optimize()
-    super_arm = [(1,10 * (t % 4)), (0,20)]    ### TEST
+    #super_arm = [(1,10 * (t % 4)), (0,20)]    ### TEST
     total_reward = 0
     for (subcampaign_id, budget_assigned) in super_arm:
         reward = environment.get_subcampaign(subcampaign_id).sample(budget_assigned)

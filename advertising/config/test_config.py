b869f992140aecf46b291c85d166c88ea551bd30
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-06-05 19:57:23 +0200

    Expanding run.py and testing it

--- advertising/config/test_config.py
+++ advertising/config/test_config.py
@@ -2,21 +2,21 @@ configuration = [
 	# Subcampaign 1
 	[{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 15,
 		'offset': 25,
 		'speed': 0.2
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 20,
 		'offset': 50,
 		'speed': 0.1
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 12,
 		'offset': 10,
 		'speed': 0.3
@@ -24,43 +24,43 @@ configuration = [
 	# Subcampaign 2
 	[{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 15,
 		'offset': 25,
 		'speed': 0.2
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
-		'max_value': 30,
+		'noise_variance': 2,
+		'max_value': 40,
 		'offset': 70,
 		'speed': 0.1
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
-		'max_value': 5,
+		'noise_variance': 2,
+		'max_value': 15,
 		'offset': 0,
 		'speed': 0.3
 	}],
 	# Subcampaign 3
 	[{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 20,
 		'offset': 25,
 		'speed': 0.5
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 20,
 		'offset': 60,
 		'speed': 0.02
 	},
 	{
 		'noise_mean': 0,
-		'noise_variance': 1,
+		'noise_variance': 2,
 		'max_value': 10,
 		'offset': 15,
 		'speed': 0.2
--- advertising/run.py
+++ advertising/run.py
@@ -14,7 +14,7 @@ import numpy as np
 ## Configurations
 ############################################
 
-timesteps = 10
+timesteps = 100
 daily_budget = 100
 budget_discretization_density = 10
 budget_discretization_steps = [i * daily_budget / budget_discretization_density for i in range(budget_discretization_density + 1)]
@@ -50,25 +50,41 @@ plt.close()
 subcampaign_algos = [Subcampaign_algo(budget_discretization_steps.copy()) for _ in range(num_subcampaigns)]
 
 ############################################
+## Build the clairvoyant solution
+############################################
+
+real_values = []
+for subcampaign in environment.subcampaigns:
+    real_values.append([subcampaign.get_real(arm) for arm in budget_discretization_steps])
+optimal_super_arm = Knapsack(daily_budget, real_values).optimize()
+print("Optimal superarm is " + str(optimal_super_arm))
+optimal_super_arm_value = sum([environment.subcampaigns[i].get_real(arm) for (i, arm) in optimal_super_arm])
+print("Value of optimal superarm = " + str(optimal_super_arm_value))
+
+############################################
 ## Main loop
 ############################################
 
 rewards = []
+clairvoyant_rewards = []
 
 for t in range(timesteps):
 
     # Sample from the subcampaign_algos to get clicks estimations
     estimations = []
     for subcampaign_algo in subcampaign_algos:
-        estimations.append([subcampaign_algo.sample_from_gp(arm) for arm in budget_discretization_steps])
+        estimate = [subcampaign_algo.sample_from_gp(arm) for arm in budget_discretization_steps]
+        if(sum(estimate) == 0):
+            estimate = [i * 1e-3 for i in range(len(budget_discretization_steps))]
+        estimate[0] = 0
+        estimations.append(estimate)
 
     # Run knapsack optimization
     super_arm = Knapsack(daily_budget, estimations).optimize()
-
-    print("-------------------------")
-    print(estimations)
-    print(super_arm)
-    print("-------------------------")
+    
+    # Fix for first day
+    if t == 0:
+        super_arm = [(i, daily_budget / num_subcampaigns) for i in range(num_subcampaigns)]
 
     # Collect rewards and update subcampaign_algos
     total_reward = 0
@@ -76,24 +92,42 @@ for t in range(timesteps):
         reward = environment.get_subcampaign(subcampaign_id).sample(budget_assigned)
         total_reward += reward
         subcampaign_algos[subcampaign_id].update(budget_assigned, reward)
-    rewards.append(total_reward)
 
-print(rewards)
+    # Collect clairvoyant rewards
+    total_clairvoyant_reward = 0
+    for (subcampaign_id, budget_assigned) in optimal_super_arm:
+        reward = environment.get_subcampaign(subcampaign_id).sample(budget_assigned)
+        total_clairvoyant_reward += reward
+    
+    print("-------------------------")
+    #print(estimations)
+    print("t = " + str(t+1) + ", superarm = " + str(super_arm) + ", reward = " + str(total_reward))
+    print("-------------------------")
+
+    rewards.append(total_reward)
+    clairvoyant_rewards.append(total_clairvoyant_reward)
 
 ############################################
 ## Plot results
 ############################################
 
 plt.plot(np.cumsum(rewards))
-plt.legend(['Cumulative reward'], bbox_to_anchor = (1.05, 1), loc = 2)
+plt.plot(np.cumsum(clairvoyant_rewards))
+plt.legend(['GPTS', 'Clairvoyant'], bbox_to_anchor = (1.05, 1), loc = 2)
 plt.title("Cumulative reward")
 plt.savefig(plot_path + 'cumulative_reward.png', bbox_inches='tight', dpi = 300)
 plt.close()
 
-i = 0
-plot_function(environment.subcampaigns[i].get_real, range(daily_budget))
-plt.plot(range(daily_budget), [subcampaign_algos[i].gaussian_process.gaussian_process.predict(np.atleast_2d(x).T) for x in range(daily_budget)])
-plt.legend(['Real click function', 'Estimated click function'], bbox_to_anchor = (1.05, 1), loc = 2)
-plt.title("Subcampaign 1")
-plt.savefig(plot_path + 'subcampaign_estimation_1.png', bbox_inches='tight', dpi = 300)
+plt.plot([(clairvoyant_rewards[i] - rewards[i]) / (i+1) for i in range(len(rewards))])
+plt.legend(['GPTS'], bbox_to_anchor = (1.05, 1), loc = 2)
+plt.title("Average regret")
+plt.savefig(plot_path + 'average_regret.png', bbox_inches='tight', dpi = 300)
 plt.close()
+
+for i in range(num_subcampaigns):
+    plot_function(environment.subcampaigns[i].get_real, range(daily_budget))
+    plt.plot(range(daily_budget), [subcampaign_algos[i].gaussian_process.gaussian_process.predict(np.atleast_2d(x).T) for x in range(daily_budget)])
+    plt.legend(['Real click function', 'Estimated click function'], bbox_to_anchor = (1.05, 1), loc = 2)
+    plt.title("Subcampaign " + str(i+1))
+    plt.savefig(plot_path + 'subcampaign_estimation_' + str(i+1) + '.png', bbox_inches='tight', dpi = 300)
+    plt.close()

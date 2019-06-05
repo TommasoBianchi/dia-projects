3f41e66d07a887a59c242a0661a84549780336de
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2019-06-05 10:26:11 +0200

    Created configuration files to manage the environment data

---
+++ advertising/config/test_config.py
@@ -0,0 +1,71 @@
+configuration = [
+	# Subcampaign 1
+	[{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 15,
+		'offset': 25,
+		'speed': 0.2
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 20,
+		'offset': 50,
+		'speed': 0.1
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 12,
+		'offset': 10,
+		'speed': 0.3
+	}],
+	# Subcampaign 2
+	[{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 15,
+		'offset': 25,
+		'speed': 0.2
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 30,
+		'offset': 70,
+		'speed': 0.1
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 5,
+		'offset': 0,
+		'speed': 0.3
+	}],
+	# Subcampaign 3
+	[{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 20,
+		'offset': 25,
+		'speed': 0.5
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 20,
+		'offset': 60,
+		'speed': 0.02
+	},
+	{
+		'noise_mean': 0,
+		'noise_variance': 1,
+		'max_value': 10,
+		'offset': 15,
+		'speed': 0.2
+	}]
+]
+
+def get_configuration():
+	return configuration
